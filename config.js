8257ec211e90f27d1441318de37cdc5fe5326291
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-06-04 13:35:54 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "GANTI DENGAN PROJECT KALIAN",
+    authDomain: "GANTI DENGAN PROJECT KALIAN",
+    databaseURL: "GANTI DENGAN PROJECT KALIAN",
+    projectId: "GANTI DENGAN PROJECT KALIAN",
+    storageBucket: "GANTI DENGAN PROJECT KALIAN",
+    messagingSenderId: "GANTI DENGAN PROJECT KALIAN",
+    appId: "GANTI DENGAN PROJECT KALIAN"
+};
+const app = firebase.initializeApp(firebaseConfig);
