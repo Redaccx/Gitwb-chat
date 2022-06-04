c10fd513ecf3100ea8dec48687e119d144f500f5
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-06-04 14:19:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAFPHSQdRugV6ngWGtPuUfSzBhS94D30hU",
-    authDomain: "reda-7d506.firebaseapp.com",
-    databaseURL: "",
-    projectId: "reda-7d506",
-    storageBucket: "reda-7d506.appspot.com",
-    messagingSenderId: "637395019512",
-    appId: "1:637395019512:web:1c7834e529eb90dcaf3d0d"
+    apiKey: "AIzaSyB8SPIHdlDqj-c3_1qHEk0PEOibm9tYYT8",
+    authDomain: "messaging761.firebaseapp.com",
+    databaseURL: "https://messaging761-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "messaging761",
+    storageBucket: "messaging761.appspot.com",
+    messagingSenderId: "766173576790",
+    appId: "1:766173576790:web:1a3905fc8cb7d0059045f7",
+    measurementId: "G-NB10CCNHXZ"
 };
+
+
 const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
+const auth = firebase.auth();
+const rdb = firebase.database();
+const stg = firebase.storage();
+const dcm = firebase.firestore();
