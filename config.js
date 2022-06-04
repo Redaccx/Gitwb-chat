36883f48d645d802ac67177bd5282cd261f7aa89
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-06-04 13:38:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "GANTI DENGAN PROJECT KALIAN",
-    authDomain: "GANTI DENGAN PROJECT KALIAN",
-    databaseURL: "GANTI DENGAN PROJECT KALIAN",
-    projectId: "GANTI DENGAN PROJECT KALIAN",
-    storageBucket: "GANTI DENGAN PROJECT KALIAN",
-    messagingSenderId: "GANTI DENGAN PROJECT KALIAN",
-    appId: "GANTI DENGAN PROJECT KALIAN"
+    apiKey: "AIzaSyAFPHSQdRugV6ngWGtPuUfSzBhS94D30hU",
+    authDomain: "reda-7d506.firebaseapp.com",
+    databaseURL: "",
+    projectId: "reda-7d506",
+    storageBucket: "reda-7d506.appspot.com",
+    messagingSenderId: "637395019512",
+    appId: "1:637395019512:web:1c7834e529eb90dcaf3d0d"
 };
 const app = firebase.initializeApp(firebaseConfig);
