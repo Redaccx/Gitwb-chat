99a69764830e8d9fba165ba07aa254acf9a935cf
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-06-29 13:43:10 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,23 @@
+// ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
 const firebaseConfig = {
-    apiKey: "AIzaSyB8SPIHdlDqj-c3_1qHEk0PEOibm9tYYT8",
-    authDomain: "messaging761.firebaseapp.com",
-    databaseURL: "https://messaging761-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "messaging761",
-    storageBucket: "messaging761.appspot.com",
-    messagingSenderId: "766173576790",
-    appId: "1:766173576790:web:1a3905fc8cb7d0059045f7",
-    measurementId: "G-NB10CCNHXZ"
+    apiKey: "PUNYA-KAMU",
+    authDomain: "PUNYA-KAMU",
+    databaseURL: "PUNYA-KAMU",
+    projectId: "PUNYA-KAMU",
+    storageBucket: "PUNYA-KAMU",
+    messagingSenderId: "PUNYA-KAMU",
+    appId: "PUNYA-KAMU"
 };
 
-
+// BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
 const app = firebase.initializeApp(firebaseConfig);
-const analytics = firebase.analytics();
 const auth = firebase.auth();
 const rdb = firebase.database();
 const stg = firebase.storage();
-const dcm = firebase.firestore();
+
+/*
+    SUBSCRIBE: DEVANKA 761 
+    https://www.youtube.com/c/RG761
+
+    IG: " @dvnkz_ "
+*/
